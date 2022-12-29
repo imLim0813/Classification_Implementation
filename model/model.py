1a445fb7b5705266aa3b88bb4e7e49da935750b0
imlim <imlim0813@hanyang.ac.kr>
2022-12-29 21:52:43 +0900

    VGGNet

--- model/model.py
+++ model/model.py
@@ -1,33 +1,49 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchinfo import summary
 
 
-class AlexNet(nn.Module):
+def VGG_Block(in_channels, out_channels, repeat):
+    vgg_block = []
+    for i in range(repeat):
+        vgg_block.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1))
+        vgg_block.append(nn.ReLU())
+        in_channels=out_channels
+    vgg_block.append(nn.MaxPool2d(kernel_size=2, stride=2))
+    return nn.Sequential(*vgg_block)
+
+
+class VGG16(nn.Module):
     def __init__(self):
-        super(AlexNet,self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4)
-        self.conv2 = nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2)
-        self.conv3 = nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1)
-        self.conv5 = nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1)
+        super(VGG16, self).__init__()
 
-        self.pool = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv_block_1 = VGG_Block(in_channels=3, out_channels=64, repeat=2)
+        self.conv_block_2 = VGG_Block(in_channels=64, out_channels=128, repeat=2)
+        self.conv_block_3 = VGG_Block(in_channels=128, out_channels=256, repeat=3)
+        self.conv_block_4 = VGG_Block(in_channels=256, out_channels=512, repeat=3)
+        self.conv_block_5 = VGG_Block(in_channels=512, out_channels=512, repeat=3)
 
-        self.fc1 = nn.Linear(in_features=256*6*6, out_features=4096)
-        self.fc2 = nn.Linear(in_features=4096, out_features=4096)
-        self.fc3 = nn.Linear(in_features=4096, out_features=10)
+        self.classifier = nn.Sequential(nn.Linear(in_features=512*7*7, out_features=4096),
+                                        nn.ReLU(),
+                                        nn.Dropout(p=0.5),
+                                        nn.Linear(in_features=4096, out_features=1024),
+                                        nn.ReLU(),
+                                        nn.Dropout(p=0.5),
+                                        nn.Linear(in_features=1024, out_features=10))
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-        x = self.pool(F.relu(self.conv5(x)))
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.conv_block_3(x)
+        x = self.conv_block_4(x)
+        x = self.conv_block_5(x)
 
         x = x.view(x.size(0), -1)
-
-        x = F.dropout(F.relu(self.fc1(x)), p=0.5)
-        x = F.dropout(F.relu(self.fc2(x)), p=0.5)
-        x = self.fc3(x)
+        x = self.classifier(x)
         return x
+
+
+if __name__ == '__main__':
+    model = VGG16()
+    summary(model, (1, 3, 224, 224))
